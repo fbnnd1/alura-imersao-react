05386b77d91ae92576a87d68a0f324a4980b264b
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2020-07-29 16:00:33 -0300

    Implementação Aula 2

--- alura-flix/src/components/Menu/index.js
+++ alura-flix/src/components/Menu/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import Logo from'../../assets/img/Logo.png';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-    return (
-
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
-            </a>
-            <a className="ButtonLink" href="/">Novo vídeo</a>
-        </nav>
-
-    );
-
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
